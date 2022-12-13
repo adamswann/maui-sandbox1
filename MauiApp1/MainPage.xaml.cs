6a7b091f86c381c5a048d78b7a1dc466089d5fc1
Adam Swann <adam@velocity2.com>
2022-12-13 08:49:10 -0600

    Add additional buttons/actions.

--- MauiApp1/MainPage.xaml.cs
+++ MauiApp1/MainPage.xaml.cs
@@ -22,5 +22,41 @@ public partial class MainPage : ContentPage
 
     }
 
+    private async void OnBrowserClicked(object sender, EventArgs e) {
+        Uri uri = new Uri("https://www.twitter.com");
+        await Browser.Default.OpenAsync(uri, BrowserLaunchMode.SystemPreferred);
+    }
+
+    private  void OnAlertClicked(object sender, EventArgs e) {
+        DisplayAlert("Alert", "Hello, World.", "OK");
+    }
+    private async void OnPickerClicked(object sender, EventArgs e)
+    {
+
+        var customFileType = new FilePickerFileType(
+                new Dictionary<DevicePlatform, IEnumerable<string>>
+                {
+                    { DevicePlatform.iOS, new[] { "public.text" } }, // UTType values
+                    { DevicePlatform.Android, new[] { "text/*" } }, // MIME type
+                    { DevicePlatform.WinUI, new[] { ".txt" } }, // file extension
+                    { DevicePlatform.Tizen, new[] { "*/*" } },
+                    { DevicePlatform.macOS, new[] { "txt" } }, // UTType values
+                });
+
+        PickOptions options = new()
+        {
+            PickerTitle = "Please select a text file",
+            FileTypes = customFileType,
+        };
+
+        var result = await FilePicker.Default.PickAsync(options);
+
+        if (result == null) {
+            await DisplayAlert("Alert", $"You picked nothing.", "OK");
+        } else { 
+            await DisplayAlert("Alert", $"You picked {result?.FileName}.", "OK");
+        }
+        
+    }
 }
 
